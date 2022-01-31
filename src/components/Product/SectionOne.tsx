4203433f84621bb175d9d26d8b0d8567e30b1e66
GustavoGomesDias <treeelevengg@gmail.com>
2022-01-31 13:10:37 -0300

    feat: add products section one

--- src/components/Product/SectionOne.tsx
+++ src/components/Product/SectionOne.tsx
@@ -1,19 +1,20 @@
 import {
+  Button,
   Flex, Grid, Image, Text,
 } from '@chakra-ui/react';
 import React from 'react';
 
 const SectionOne = (): JSX.Element => {
   return (
-    <Flex w="100%" justifyContent="center">
+    <Flex w="100%" justifyContent="center" flexDir="column" alignItems="center" mb="5px">
       <Flex w="70%">
         <Image w="60%" src="/images/secOne.svg" alt="Product description representation image" />
-        <Grid templateRows="repeat(5, 0.5fr)" gap={2}>
-          <Text fontSize="24px">
+        <Grid templateRows="repeat(5, 0.5fr)" gap={2} ml="1em">
+          <Text fontSize="24px" mt="1.8em">
             CONHEÇA seu <span style={{ fontWeight: 'bold' }}> público </span>
           </Text>
           <Text fontSize="24px">
-            ACOMPANHE suas vendas em <span style={{ fontWeight: 'bold' }}> qualquer lugar</span>
+            ACOMPANHE suas <span style={{ fontWeight: 'bold' }}> vendas</span> em <span style={{ fontWeight: 'bold' }}> qualquer lugar</span>
           </Text>
           <Text fontSize="24px">
             VENDAS online com <span style={{ fontWeight: 'bold' }}> eficiência máxixa</span>
@@ -23,6 +24,19 @@ const SectionOne = (): JSX.Element => {
           </Text>
         </Grid>
       </Flex>
+      <Button
+        size="md"
+        borderRadius="0px"
+        fontWeight="medium"
+        px="2em"
+        bg="#34b4ac"
+        color="#fff"
+        _hover={{
+          bg: '#28a199',
+        }}
+      >
+        Fale direto com um especialista
+      </Button>
     </Flex>
   );
 };
