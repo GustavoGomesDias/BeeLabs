b64a7cc9c936da6b5b9c8bad06dc33667b79f777
GustavoGomesDias <treeelevengg@gmail.com>
2022-01-31 16:47:01 -0300

    feat: add responsive style in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import {
-  Button, ButtonGroup, chakra, Image,
+  Button, ButtonGroup, chakra, Image, useMediaQuery,
 } from '@chakra-ui/react';
 
 const Header = (): JSX.Element => {
+  const [isSmallScreen] = useMediaQuery('(max-width: 768px)');
   return (
     <chakra.header
       display="flex"
       alignItems="center"
       justifyContent="space-between"
       w="100%"
-      p={8}
+      p={isSmallScreen ? 4 : 8}
     >
-      <Image ml="10%" src="/images/logo.svg" w="30%" h="20%" alt="Bee CMR logo" />
-      <ButtonGroup px={6} display="flex" justifyContent="flex-end">
+      <Image ml={isSmallScreen ? '2%' : '10%'} src="/images/logo.svg" w="30%" h="20%" alt="Bee CMR logo" />
+      <ButtonGroup px={2} display="flex" justifyContent="flex-end">
         <Button size="xs" borderRadius="0px" variant="link">CONTATO</Button>
         <Button size="xs" borderRadius="0px" borderWidth="1px" borderColor="#34b4ac" bg="#fff">LOGIN</Button>
         <Button size="xs" borderRadius="0px" borderWidth="1px" borderColor="#34b4ac" bg="#fff">TESTE GRÁTIS</Button>
