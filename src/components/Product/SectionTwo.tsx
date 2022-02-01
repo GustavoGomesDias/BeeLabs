8ab17e71388dfa6dda76790003ac87b2997ec473
GustavoGomesDias <treeelevengg@gmail.com>
2022-02-01 16:44:19 -0300

    feat: transform section two in responsive

--- src/components/Product/SectionTwo.tsx
+++ src/components/Product/SectionTwo.tsx
@@ -1,6 +1,6 @@
 import {
   Button,
-  chakra, Flex, Text, useMediaQuery
+  chakra, Flex, Text, useMediaQuery,
 } from '@chakra-ui/react';
 import React from 'react';
 import SecTwoList from './SecTwoList';
@@ -35,12 +35,39 @@ const SectionTwo = (): JSX.Element => {
         </Button>
         <Text fontSize="14px" textAlign="center" mt="0.5em">* Teste gratuito de 7 dias</Text>
       </Flex>
-      <Flex mt="2.5em" w="90%">
-        <Flex w="100%" justifyContent="center" bgImage="/images/picture1.svg" mr="1em" bgSize="100%" bgPos="center" bgRepeat="no-repeat" h="600px">
-          <Text fontSize="30px" textAlign="center" fontWeight="bold" w="50%" mt="5%" color="#042454">TODOS OS DEPARTAMENTOS DA SUA EMPRESA CONECTADOS</Text>
+      <Flex mt="2.5em" w="90%" flexDir={isSmallScreen ? 'column' : 'row'}>
+        <Flex
+          w="100%"
+          justifyContent="center"
+          bgImage="/images/picture1.svg"
+          mr="1em"
+          bgSize="100%"
+          bgPos="center"
+          bgRepeat="no-repeat"
+          h={isSmallScreen ? '400px' : '600px'}
+        >
+          <Text
+            fontSize={isSmallScreen ? '20px' : '30px'}
+            textAlign="center"
+            fontWeight="bold"
+            w="50%"
+            mt="5%"
+            color="#042454"
+          >
+            TODOS OS DEPARTAMENTOS DA SUA EMPRESA CONECTADOS
+          </Text>
         </Flex>
-        <Flex w="100%" justifyContent="center" bgImage="/images/picture2.svg" bgSize="100%" bgPos="center" bgRepeat="no-repeat" h="600px">
-          <Text fontSize="30px" textAlign="center" fontWeight="bold" w="50%" mt="5%" color="#fff">TREINAMENTO COMPLETO E SUPORTE 24h</Text>
+        <Flex
+          w="100%"
+          justifyContent="center"
+          bgImage="/images/picture2.svg"
+          bgSize="100%"
+          bgPos="center"
+          bgRepeat="no-repeat"
+          h={isSmallScreen ? '400px' : '600px'}
+          mt={isSmallScreen ? '10%' : 'none'}
+        >
+          <Text fontSize={isSmallScreen ? '20px' : '30px'} textAlign="center" fontWeight="bold" w="50%" mt="5%" color="#fff">TREINAMENTO COMPLETO E SUPORTE 24h</Text>
         </Flex>
       </Flex>
     </Flex>
