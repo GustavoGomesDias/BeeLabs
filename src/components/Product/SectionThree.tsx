6935f9fdc8c162d6d068e9999510c6f0782270b5
GustavoGomesDias <treeelevengg@gmail.com>
2022-02-01 16:57:12 -0300

    feat: transform sect three in responsive

--- src/components/Product/SectionThree.tsx
+++ src/components/Product/SectionThree.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {
-  chakra, Flex, Image, UnorderedList, ListItem, Button, Text,
+  chakra, Flex, Image, UnorderedList, ListItem, Button, Text, useMediaQuery,
 } from '@chakra-ui/react';
 
 const SectionThree = (): JSX.Element => {
+  const [isSmallScreen] = useMediaQuery('(max-width: 768px)');
+
   return (
     <Flex w="full" alignItems="center" flexDir="column">
       <Flex w="full" bg="#34b4ac" flexDir="column">
         <Flex flexDir="column" alignItems="center" w="full">
-          <chakra.h1 fontSize="48px" fontWeight="bold" color="#042454">ANÁLISES INTELIGENTEMENTE SIMPLES</chakra.h1>
-          <chakra.h3 fontSize="28px" color="#fff">Um app que vive trabalhando para você vender mais</chakra.h3>
+          <chakra.h1 textAlign={isSmallScreen ? 'center' : 'unset'} fontSize={isSmallScreen ? '26px' : '48px'} fontWeight="bold" color="#042454">ANÁLISES INTELIGENTEMENTE SIMPLES</chakra.h1>
+          <chakra.h3 textAlign={isSmallScreen ? 'center' : 'unset'} fontSize={isSmallScreen ? '24px' : '28px'} color="#fff">Um app que vive trabalhando para {isSmallScreen && <br />} você vender mais</chakra.h3>
         </Flex>
-        <Flex w="full" justifyContent="center">
-          <Image src="/images/secThree.svg" about="Spreadsheet representation" w="40%" />
-          <UnorderedList fontSize="24px" w="40%" spacing={3} color="#fff" display="flex" flexDir="column" justifyContent="center">
+        <Flex w="full" justifyContent="center" alignItems="center" flexDir={isSmallScreen ? 'column' : 'row'}>
+          <Image src="/images/secThree.svg" about="Spreadsheet representation" w={isSmallScreen ? '90%' : '40%'} />
+          <UnorderedList p="2em" fontSize="20px" w={isSmallScreen ? '100%' : '40%'} spacing={3} color="#fff" display="flex" flexDir="column" justifyContent="center">
             <ListItem><span style={{ fontWeight: 'bold', color: 'black' }}>Relatórios</span> completos da atividade</ListItem>
             <ListItem><span style={{ fontWeight: 'bold', color: 'black' }}>Métricas</span> de desempenho</ListItem>
             <ListItem>Análise de <span style={{ fontWeight: 'bold', color: 'black' }}>tendências</span> (fale com a pessoa certa na hora certa)</ListItem>
@@ -23,7 +25,7 @@ const SectionThree = (): JSX.Element => {
         </Flex>
       </Flex>
       <Button
-        size="lg"
+        size={isSmallScreen ? 'md' : 'lg'}
         borderRadius="0px"
         fontWeight="medium"
         px="2em"
