a6c79b704aae9ead349812d88d0d5565dc1b6a07
GustavoGomesDias <treeelevengg@gmail.com>
2022-02-03 13:36:56 -0300

    fix: adjust sec one to a md screnen

--- src/components/Product/SectionOne.tsx
+++ src/components/Product/SectionOne.tsx
@@ -7,10 +7,11 @@ import { FaCheck } from 'react-icons/fa';
 
 const SectionOne = (): JSX.Element => {
   const [isSmallScreen] = useMediaQuery('(max-width: 768px)');
+  const [isMdScreen] = useMediaQuery('(max-width: 1000px)');
 
   return (
     <Flex w="100%" justifyContent="center" flexDir="column" alignItems="center" mb="5px">
-      <Flex w="70%">
+      <Flex w={isMdScreen && !isSmallScreen ? '90%' : '70%'}>
         {!isSmallScreen && (
           <Image
             w="60%"
@@ -18,14 +19,14 @@ const SectionOne = (): JSX.Element => {
             alt="Product description representation image"
           />
         )}
-        <Grid templateRows="repeat(5, 0.5fr)" gap={2} ml="1em">
+        <Grid templateRows="repeat(5, 0.5fr)" gap={isMdScreen ? 0 : 2} ml="1em">
           <Flex alignItems="center" mt="1.8em">
             {isSmallScreen && (
               <Flex mr="15px">
                 <FaCheck size="20px" color="#34b4ac" style={{ marginRight: '5px' }} />
               </Flex>
             )}
-            <Text fontSize={isSmallScreen ? '18px' : '24px'}>
+            <Text fontSize={isSmallScreen || isMdScreen ? '18px' : '24px'}>
               CONHEÇA seu <span style={{ fontWeight: 'bold', color: 'black' }}> público</span>
             </Text>
           </Flex>
@@ -35,7 +36,7 @@ const SectionOne = (): JSX.Element => {
                 <FaCheck size="20px" color="#34b4ac" style={{ marginRight: '5px' }} />
               </Flex>
             )}
-            <Text fontSize={isSmallScreen ? '18px' : '24px'}>
+            <Text fontSize={isSmallScreen || isMdScreen ? '18px' : '24px'}>
               ACOMPANHE suas <span style={{ fontWeight: 'bold', color: 'black' }}> vendas</span> em <span style={{ fontWeight: 'bold', color: 'black' }}> qualquer lugar</span>
             </Text>
           </Flex>
@@ -45,7 +46,7 @@ const SectionOne = (): JSX.Element => {
                 <FaCheck size="20px" color="#34b4ac" style={{ marginRight: '5px' }} />
               </Flex>
             )}
-            <Text fontSize={isSmallScreen ? '18px' : '24px'}>
+            <Text fontSize={isSmallScreen || isMdScreen ? '18px' : '24px'}>
               VENDAS online com <span style={{ fontWeight: 'bold', color: 'black' }}> eficiência máxixa</span>
             </Text>
           </Flex>
@@ -55,7 +56,7 @@ const SectionOne = (): JSX.Element => {
                 <FaCheck size="20px" color="#34b4ac" style={{ marginRight: '5px' }} />
               </Flex>
             )}
-            <Text fontSize={isSmallScreen ? '18px' : '24px'}>
+            <Text fontSize={isSmallScreen || isMdScreen ? '18px' : '24px'}>
               FIDELIZE clientes com <span style={{ fontWeight: 'bold', color: 'black' }}> insights preciosos</span>
             </Text>
           </Flex>
